eef77c84d1ea898d6431498eb5a3fa2625f8c6cd
Seemann <mail@sannybuilder.com>
2019-09-02 23:23:29 -0400

    formatting

--- clipboard.js
+++ clipboard.js
@@ -1,50 +1,43 @@
 // JavaScript Document
-function ClipBoard() 
-{
-holdtext.innerText = copytext.innerText;
-Copied = holdtext.createTextRange();
-Copied.execCommand("RemoveFormat");
-Copied.execCommand("Copy");
+function ClipBoard() {
+  holdtext.innerText = copytext.innerText;
+  Copied = holdtext.createTextRange();
+  Copied.execCommand("RemoveFormat");
+  Copied.execCommand("Copy");
 }
-function ClipBoard1() 
-{
-holdtext1.innerText = copytext1.innerText;
-Copied = holdtext1.createTextRange();
-Copied.execCommand("RemoveFormat");
-Copied.execCommand("Copy");
+function ClipBoard1() {
+  holdtext1.innerText = copytext1.innerText;
+  Copied = holdtext1.createTextRange();
+  Copied.execCommand("RemoveFormat");
+  Copied.execCommand("Copy");
 }
-function ClipBoard2() 
-{
-holdtext2.innerText = copytext2.innerText;
-Copied = holdtext2.createTextRange();
-Copied.execCommand("RemoveFormat");
-Copied.execCommand("Copy");
+function ClipBoard2() {
+  holdtext2.innerText = copytext2.innerText;
+  Copied = holdtext2.createTextRange();
+  Copied.execCommand("RemoveFormat");
+  Copied.execCommand("Copy");
 }
-function ClipBoard3() 
-{
-holdtext3.innerText = copytext3.innerText;
-Copied = holdtext3.createTextRange();
-Copied.execCommand("RemoveFormat");
-Copied.execCommand("Copy");
+function ClipBoard3() {
+  holdtext3.innerText = copytext3.innerText;
+  Copied = holdtext3.createTextRange();
+  Copied.execCommand("RemoveFormat");
+  Copied.execCommand("Copy");
 }
-function ClipBoard4() 
-{
-holdtext4.innerText = copytext4.innerText;
-Copied = holdtext4.createTextRange();
-Copied.execCommand("RemoveFormat");
-Copied.execCommand("Copy");
+function ClipBoard4() {
+  holdtext4.innerText = copytext4.innerText;
+  Copied = holdtext4.createTextRange();
+  Copied.execCommand("RemoveFormat");
+  Copied.execCommand("Copy");
 }
-function ClipBoard5() 
-{
-holdtext5.innerText = copytext5.innerText;
-Copied = holdtext5.createTextRange();
-Copied.execCommand("RemoveFormat");
-Copied.execCommand("Copy");
+function ClipBoard5() {
+  holdtext5.innerText = copytext5.innerText;
+  Copied = holdtext5.createTextRange();
+  Copied.execCommand("RemoveFormat");
+  Copied.execCommand("Copy");
 }
-function ClipBoard6() 
-{
-holdtext6.innerText = copytext6.innerText;
-Copied = holdtext6.createTextRange();
-Copied.execCommand("RemoveFormat");
-Copied.execCommand("Copy");
+function ClipBoard6() {
+  holdtext6.innerText = copytext6.innerText;
+  Copied = holdtext6.createTextRange();
+  Copied.execCommand("RemoveFormat");
+  Copied.execCommand("Copy");
 }
